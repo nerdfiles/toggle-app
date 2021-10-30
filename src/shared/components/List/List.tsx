ae4b9ccbd4bdc71bbd55ed24143306a4e2fc6da5
nerdfiles <nerdfiles@gmail.com>
2021-10-30 18:25:13 -0500

    notes on init state

--- src/shared/components/List/List.tsx
+++ src/shared/components/List/List.tsx
@@ -2,6 +2,15 @@
  * @fileoverview ./src/shared/components/List/List.tsx
  * @description
  * A list container component.
+ *
+ * listDataFromApi could be a list of objects instead of "stateful" strings, 
+ * which are loaded via pubsub (think resthooks.org) for async REST with HATEOAS 
+ * or just by some async promise-work; e.g., 
+ * ```
+ * [{ status: 'off', content: 'blah blah' }, { status: 'off', content: 'blah blah' }, ...]
+ * ```
+ * then map over the objects, check the status property for meaningful truth 
+ * or whatever hierarchicalization you wish in business logic
  */
 import React, { useState } from 'react';
 import apiData from '../../services/api'
