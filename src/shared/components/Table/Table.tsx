189a0089f0d29e4d1f837761aebd18cf1e6a7a92
nerdfiles <nerdfiles@gmail.com>
2021-10-30 17:50:13 -0500

    'cracked out code'

---
+++ src/shared/components/Table/Table.tsx
@@ -0,0 +1,28 @@
+/**
+ * @fileoverview ./src/shared/components/Table/Table.tsx
+ * @description
+ * Custom table component.
+ */
+
+const Table: React.FC = () => {
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+          </tr>
+        </thead>
+        <tfoot>
+          <caption></caption>
+        </tfoot>
+        <tbody>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default Table
+
+// EOF
